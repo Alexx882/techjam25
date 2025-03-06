102f4b812b5bcde494dfb2127fac2e54747476f4
Alexander Lercher <alexander.lercher@aau.at>
2025-03-06 16:52:33 -0500

    Extract string constants for homepage

---
+++ jam-specific-content.js
@@ -0,0 +1,16 @@
+const gamejamName = "Klagenfurt Game Jam, Tech Edition"
+const gamejamNameAndDate = gamejamName + ", April 11-13, 2025"
+const registrationFormURL = "http://example.com"
+const itchIoUrl = "https://itch.io/jam/xxx"
+const discordUrl = "https://discord.gg/4BWEHGm4nN"
+const previousJamsUrl = "https://www.itec.aau.at/gamejam/"
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.placeholder-jam-title').forEach(el => el.textContent = gamejamName);
+    document.querySelectorAll('.placeholder-carousel-jam-title').forEach(el => el.textContent = gamejamName);
+    document.querySelectorAll('.placeholder-jam-title-date').forEach(el => el.textContent = gamejamNameAndDate);
+    document.querySelectorAll('.placeholder-registration-form-url').forEach(el => el.href = registrationFormURL);
+    document.querySelectorAll('.placeholder-itch-io-url').forEach(el => el.href = itchIoUrl);
+    document.querySelectorAll('.placeholder-discord-url').forEach(el => el.href = discordUrl);
+    document.querySelectorAll('.placeholder-previous-jams-url').forEach(el => el.href = previousJamsUrl);
+});
